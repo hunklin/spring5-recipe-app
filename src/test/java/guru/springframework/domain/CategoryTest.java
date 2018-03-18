f4cca574382feac0942caf9801e33b208aade700
Hunk Lin <ll6464@sina.com>
2018-03-18 21:18:28 +0800

    assignment: mock test

---
+++ src/test/java/guru/springframework/domain/CategoryTest.java
@@ -0,0 +1,26 @@
+package guru.springframework.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CategoryTest {
+
+    Category category;
+    @Before
+    public void setUp(){
+        category = new Category();
+    }
+    @Test
+    public void getId() throws Exception {
+        Long idValue = 3L;
+        category.setId(idValue);
+        assertEquals(idValue, category.getId());
+    }
+
+    @Test
+    public void getDescription() throws Exception {
+    }
+
+}
